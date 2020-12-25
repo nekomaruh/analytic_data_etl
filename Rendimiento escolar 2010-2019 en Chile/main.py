6decbdd00d3982ddfe2793867b82953618231140
Johan Esteban <johesteb@gmail.com>
2020-12-24 23:35:41 -0300

    update

--- Rendimiento escolar 2010-2019 en Chile/main.py
+++ Rendimiento escolar 2010-2019 en Chile/main.py
@@ -5,16 +5,34 @@ import numpy as np
 import re
 import time
 
+#from static_tables import *
+
 start_time = time.time()
 
-# Leemos los datos y separamos por ; porque algunos nombres de establecimientos poseen comas y dan error
-df_2010 = pd.read_csv("Rendimiento por estudiante 2010.csv", sep=';')
-df_len = df_2010['NOM_RBD'].count()
+def read_file(year, drops):
+    # Leemos los datos y separamos por ; porque algunos nombres de establecimientos poseen comas y dan error
+    df = pd.read_csv("Rendimiento por estudiante "+str(year)+".csv", sep=';', low_memory=False)
+    df_len = df['NOM_RBD'].count()
 
-# Eliminamos columna de Código de sector económico (sólo media técnico-profesional y artística)
-# Eliminamos columna de Código de especialidad (sólo media técnico-profesional y artística)
-# No nos sirve para el análisis de datos final y contienen datos vacíos en algunos casos
-df_2010 = df_2010.drop(columns=['COD_SEC', 'COD_ESPE', 'INT_ALU'])
+    # Eliminamos columna de Código de sector económico (sólo media técnico-profesional y artística)
+    # Eliminamos columna de Código de especialidad (sólo media técnico-profesional y artística)
+    # No nos sirve para el análisis de datos final y contienen datos vacíos en algunos casos
+    df = df.drop(columns=drops)
+    return df
 
+# Leemos los archivos
+df_2010 = read_file(year=2010, drops=['COD_SEC', 'COD_ESPE', 'INT_ALU'])
+#df_2011 = read_file(year=2011, drops=['COD_SEC', 'COD_ESPE', 'INT_ALU', 'FEC_ING_ALU'])
+#df_2012 = read_file(year=2012, drops=['COD_SEC', 'COD_ESPE', 'INT_ALU'])
+
+# Exportamos archivos html para ver si las tablas están bien
+#html_2010 = df_2010.to_html("df_2010.html")
+
+
+#print(df_2010)
+
+for col in df_2010.columns: 
+    print(col)
+    
 print('Termino')
-print(df_2010)
+
